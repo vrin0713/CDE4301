9b25d4ca707de65018d7d03c515992171bd8cb16
vrin0713 <145411453+vrin0713@users.noreply.github.com>
2024-09-08 15:45:13 +0800

    Update table-component.js

--- components/table-component/table-component.js
+++ components/table-component/table-component.js
@@ -1,11 +1,12 @@
 new gridjs.Grid({
-  columns: ["Name", "Email", "Phone Number"],
+  columns: ["Key Functional Requirements", "Details / Rationale"],
   data: [
-    ["John", "john@example.com", "(353) 01 222 3333"],
-    ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-    ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-    ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-    ["Afshin", "afshin@mail.com", "(353) 22 87 8356"],
+    ["Controllable and known light intensities", "The device must provide controlled and variable light intensities to assess the eye’s response and discomfort levels at different intensities. This will help determine the threshold at which light triggers photophobia."],
+    ["Varying features of light stimulus ", "The light stimulus needs to be controlled to vary the frequency and wavelengths (colours)"],
+    ["Real-time Data Analysis:", "The device should be capable of detecting and measuring physiological responses (e.g., pupil dilation, Blinking frequency) "],
+    ["Mobile App Integration", "Integrating the device with a mobile app will allow for easy data collection, storage, and visualization. The app will provide an interface for ophthalmologists control the different light stimulus they want to emit into the patients eye "],
+    ["Ease of Use:", "The device must be user-friendly for both patients and healthcare providers, with intuitive controls and minimal setup required "],
+    ['Safety Features','Since the device will emit light directly into patients’ eyes. The device must not exceed certain intensity safety levels'],
   ],
 }).render(document.getElementById("table-1"));
 
